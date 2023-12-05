af22f09705f0bd976129f7914ca71df27a6b4920
Paul Booker <paul@paulbooker.co.uk>
2023-12-05 17:14:52 +0000

    udemy-symfony-micropost-69

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,15 @@
 	
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\MicroPost;
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\CommentRepository;
+use App\Repository\MicroPostRepository;
+use App\Repository\UserProfileRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,14 +23,51 @@ class HelloController extends AbstractController
 	    ['message' => 'Bye!', 'created' => '2022/05/12']
 	];
 	
-	#[Route('/{limit<\d+>?3}', name: 'app_index')]
-	public function index(int $limit): Response
+	#[Route('/', name: 'app_index')]
+	public function index(MicroPostRepository $posts, CommentRepository $comments, UserProfileRepository $profiles, EntityManagerInterface $entityManager): Response
 	{
+		//$post  = new MicroPost();
+		//$post->setTitle('Hello');
+		//$post->setText('Hello');
+		//$post->setCreated(new DateTime());
+		
+		//$post = $posts->find(11);
+		//$comment = $post->getComments()[0];
+		
+		//$post->removeComment($comment);
+
+		//$entityManager->persist($post);
+	    //$entityManager->flush();
+		
+		//dd($post);
+		
+		//$comment = new Comment();
+		//$comment->setText('Hello');
+		//$comment->setPost($post);
+		//$post->addComment($comment);
+		
+		//$entityManager->persist($comment);
+	    //$entityManager->flush();
+		
+		
+		//$user = new User();
+		//$user->setEmail('somebody@somewhere.com');
+		//$user->setPassword('password');
+		
+		//$profile = new UserProfile();
+		//$profile->setUser($user);
+		//$entityManager->persist($profile);
+	    //$entityManager->flush();
+		
+		//$profile = $profiles->find(1);
+		//$entityManager->remove($profile);
+	    //$entityManager->flush();
+		
 		return $this->render(
 			'hello/index.html.twig',
 			[
 				'messages' => $this->messages,
-				'limit' => $limit
+				'limit' => 3
 			]
 		);
 		
--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\MicroPost;
+use App\Form\CommentType;
 use App\Form\MicroPostType;
+use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
@@ -41,7 +44,6 @@ class MicroPostController extends AbstractController
 	    if ($form->isSubmitted() && $form->isValid()) {
 		    $post = $form->getData();
 		    $post->setCreated(new DateTime());
-		    //$posts->add($post, true);
 		    $entityManager->persist($post);
 			$entityManager->flush();
 		    
@@ -83,6 +85,39 @@ class MicroPostController extends AbstractController
 		    'micro_post/edit.html.twig',
 		    [
 			    'form' => $form
+		    ]
+	    );
+    }
+
+	#[Route('micro-post/{post}/comment', name: 'app_micro_post_comment')]
+    public function addComment(MicroPost $post, Request $request, CommentRepository $comments, EntityManagerInterface $entityManager): Response
+    {
+	    $form = $this->createForm(CommentType::class, new Comment());
+	    	
+	    $form->handleRequest($request);
+	    
+	    if ($form->isSubmitted() && $form->isValid()) {
+		    $comment = $form->getData();
+		    $comment->setPost($post);
+		    
+		    $entityManager->persist($comment);
+			$entityManager->flush();
+		    
+		    // Add a flash message
+		    $this->addFlash('success', 'Your comment has been updated.');
+		    
+		    // Redirect
+		    return $this->redirectToRoute(
+		    	'app_micro_post_show',
+		    	['post' => $post->getId()]
+		    	);
+	    }
+	    
+	    return $this->renderForm(
+		    'micro_post/comment.html.twig',
+		    [
+			    'form' => $form,
+			    'post' => $post
 		    ]
 	    );
     }
